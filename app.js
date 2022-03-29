6b10729e41cffa3788621db95132c153dd5c801a
alex Campbell <alexbe.campbell@gmail.com>
2022-03-28 23:55:20 -0500

    console log practice & let/const declarations

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
-alert('HELLO WORLD');
+// var name = 'John Doe';
+// console.log(name);
+
+// name = 'Steve'
+// console.log(name);
+
+
+// //Init variables
+// var greeting;
+// console.log(greeting)
+
+// greeting = 'HELLOW'
+// console.log(greeting)
+
+// var firstName = 'Joshn'
+// var lastName = 'DOES'
+
+// console.log(firstName + ' ' + lastName);
+
+const person = {
+    name: 'Johsn',
+    age: 2
+}
+
+person.name = 'GLEEBA JOPE'
+console.log(person);
+
+const numbers = [1,2,3,4,5,6];
+numbers.unshift();
+console.log(numbers);
