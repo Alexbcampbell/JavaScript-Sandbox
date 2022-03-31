84102022129b87ffab2083dd080692cedc068916
alex Campbell <alexbe.campbell@gmail.com>
2022-03-31 00:18:44 -0500

    type conversion practice

--- app.js
+++ app.js
@@ -57,3 +57,46 @@
 // const today = new Date();
 // console.log(today);
 // console.log(typeof today);
+
+// let val;
+
+// //number to string
+// val = String(5);
+// val = String(4+4);
+// //boolean
+// val = String(true);
+// //date
+// val = String(new Date);
+// //array
+// val = String([1,2,3,4]);
+
+// //toString()
+// val = (5).toString();
+// val = (true).toString();
+// //string to number
+// val = Number('5');
+// val = Number(true);
+// val = Number(false);
+// val = Number(null);
+// val = Number('Hello');
+// val = Number([1,2,3]);
+
+// //parseInt
+// val = parseInt('100');
+// val = parseFloat('100.2443');
+
+// //output
+// console.log(val);
+// console.log(typeof val);
+// //console.log(val.length);
+// console.log(val.toFixed(2));
+
+
+//type coersion - concatinating strings and numbers
+
+const val1 = String(5);
+const val2 = 6;
+const sum = val1 + val2;
+
+console.log(sum);
+console.log(typeof sum);
