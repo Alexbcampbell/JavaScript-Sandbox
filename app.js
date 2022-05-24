7fc76d072e86fca0e343eda39e0b765c938cc42b
alex Campbell <alexbe.campbell@gmail.com>
2022-05-24 01:28:18 -0500

    Constructor Method/this  practice

--- app.js
+++ app.js
@@ -696,3 +696,23 @@ return x*x
 // // }
 
 // console.log('Global scope ' , a , b, c)
+
+//Person constructor
+function Person(name, dob){
+    this.name = name;
+    this.birthday = new Date(dob);
+    this.calculateAge = function(){
+        const diff = Date.now() - this.birthday.getTime();
+        const ageDate = new Date(diff);
+        return Math.abs(ageDate.getUTCFullYear() - 1970);
+    }
+    // this.age = age
+    //console.log(this);
+}
+
+// const brad = new Person('Brad', 30);
+// const john = new Person('John', 36);
+
+const brad = new Person('Brad', '9-10-1988');
+
+console.log(brad.calculateAge());
