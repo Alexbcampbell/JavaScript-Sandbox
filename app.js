7fd1433a7e22bb9526edd72a2407504b5f36458c
alex Campbell <alexbe.campbell@gmail.com>
2022-05-25 23:59:04 -0500

    Sub classes and extending classes practice

--- app.js
+++ app.js
@@ -842,35 +842,67 @@ return x*x
 // console.log(brad)
 // console.log(brad.greeting())
 
+// class Person {
+//     constructor(firstName, lastName, dob){
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//         this.birthday = new Date(dob);
+//     }
+
+//     greeting(){
+//         return `Hello there ${this.firstName} ${this.lastName}`
+//     }
+
+//     calculateAge(){
+//         const diff = Date.now() - this.birthday.getTime();
+//         const ageDate = new Date(diff);
+//         return Math.abs(ageDate.getUTCFullYear() - 1970);
+//     }
+
+//     getsMarried(newLastName) {
+//         this.lastName = newLastName;
+//     }
+
+//     static addNumbers(x,y) {
+//         return x + y;
+//     }
+// }
+
+// const mary = new Person('Mary', 'Williams', '11-13-1980');
+
+// mary.getsMarried('Thompson');
+// console.log(mary)
+
+// console.log(Person.addNumbers(1,2));
+
+//SUB CLASSES
 class Person {
-    constructor(firstName, lastName, dob){
+    constructor(firstName, lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.birthday = new Date(dob);
+
     }
 
     greeting(){
-        return `Hello there ${this.firstName} ${this.lastName}`
-    }
-
-    calculateAge(){
-        const diff = Date.now() - this.birthday.getTime();
-        const ageDate = new Date(diff);
-        return Math.abs(ageDate.getUTCFullYear() - 1970);
-    }
-
-    getsMarried(newLastName) {
-        this.lastName = newLastName;
-    }
-
-    static addNumbers(x,y) {
-        return x + y;
+        return `Hello there ${this.firstName} ${this.lastName}`;
     }
 }
 
-const mary = new Person('Mary', 'Williams', '11-13-1980');
+class Customer extends Person {
+    constructor(firstName, lastName, phone, membership){
+        super(firstName, lastName);
 
-mary.getsMarried('Thompson');
-console.log(mary)
+        this.phone = phone;
+        this.membership = membership;
+    }
 
-console.log(Person.addNumbers(1,2));
+    static getMembershipCost(){
+        return 500;
+    }
+}
+
+const john = new Customer('John', 'Doe', '555-5555', 'Standard');
+
+console.log(john.greeting())
+
+console.log(Customer.getMembershipCost());
