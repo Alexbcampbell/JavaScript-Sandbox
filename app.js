8c6c73646ed668387b26d742ab84c19b8214e608
alex Campbell <alexbe.campbell@gmail.com>
2022-05-25 23:52:40 -0500

    ES6 Class and static method practice

--- app.js
+++ app.js
@@ -815,29 +815,62 @@ return x*x
 // console.log(customer1.greeting());
 
 //Object.create
-const personPrototypes = {
-    greeting: function(){
-        return `Hello there ${this.firstName} ${this.lastName}`;
-    },
-    getsMarried: function(newLastName){
-        this.lastName = newLastName
+// const personPrototypes = {
+//     greeting: function(){
+//         return `Hello there ${this.firstName} ${this.lastName}`;
+//     },
+//     getsMarried: function(newLastName){
+//         this.lastName = newLastName
+//     }
+// }
+
+// const mary = Object.create(personPrototypes);
+// mary.firstName = 'Mary';
+// mary.lastName = 'Williams';
+// mary.age = 30;
+
+// mary.getsMarried('Thompson');
+
+// console.log(mary.greeting());
+
+// const brad = Object.create(personPrototypes, {
+//     firstName: {value: 'Brad'},
+//     lastName: {value: 'Travers'},
+//     age: {value: 36}
+// })
+
+// console.log(brad)
+// console.log(brad.greeting())
+
+class Person {
+    constructor(firstName, lastName, dob){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthday = new Date(dob);
+    }
+
+    greeting(){
+        return `Hello there ${this.firstName} ${this.lastName}`
+    }
+
+    calculateAge(){
+        const diff = Date.now() - this.birthday.getTime();
+        const ageDate = new Date(diff);
+        return Math.abs(ageDate.getUTCFullYear() - 1970);
+    }
+
+    getsMarried(newLastName) {
+        this.lastName = newLastName;
+    }
+
+    static addNumbers(x,y) {
+        return x + y;
     }
 }
 
-const mary = Object.create(personPrototypes);
-mary.firstName = 'Mary';
-mary.lastName = 'Williams';
-mary.age = 30;
+const mary = new Person('Mary', 'Williams', '11-13-1980');
 
 mary.getsMarried('Thompson');
+console.log(mary)
 
-console.log(mary.greeting());
-
-const brad = Object.create(personPrototypes, {
-    firstName: {value: 'Brad'},
-    lastName: {value: 'Travers'},
-    age: {value: 36}
-})
-
-console.log(brad)
-console.log(brad.greeting())
+console.log(Person.addNumbers(1,2));
