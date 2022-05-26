910d1d027696497d4e4e1814936881f3d410f348
alex Campbell <alexbe.campbell@gmail.com>
2022-05-25 23:42:56 -0500

    Object.creat practice

--- app.js
+++ app.js
@@ -813,3 +813,31 @@ return x*x
 // }
 
 // console.log(customer1.greeting());
+
+//Object.create
+const personPrototypes = {
+    greeting: function(){
+        return `Hello there ${this.firstName} ${this.lastName}`;
+    },
+    getsMarried: function(newLastName){
+        this.lastName = newLastName
+    }
+}
+
+const mary = Object.create(personPrototypes);
+mary.firstName = 'Mary';
+mary.lastName = 'Williams';
+mary.age = 30;
+
+mary.getsMarried('Thompson');
+
+console.log(mary.greeting());
+
+const brad = Object.create(personPrototypes, {
+    firstName: {value: 'Brad'},
+    lastName: {value: 'Travers'},
+    age: {value: 36}
+})
+
+console.log(brad)
+console.log(brad.greeting())
