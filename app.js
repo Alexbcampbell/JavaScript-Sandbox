a79532b872bba99f11271ee0f4a85dc2c31ebb04
alex Campbell <alexbe.campbell@gmail.com>
2022-05-25 23:35:09 -0500

    prototype inheritance practice

--- app.js
+++ app.js
@@ -770,3 +770,46 @@ return x*x
 //each object in javascript has a prototype
 //object.prototype
 //person.prototype
+
+
+//prototypal inheritance
+//Person
+// function Person(firstName, lastName){
+// this.firstName = firstName;
+// this.lastName = lastName;
+// }
+
+// //Greeting
+// Person.prototype.greeting = function(){
+//     return `Hello there ${this.firstName} ${this.lastName}`;
+// }
+
+// const person1 = new Person('John', 'Doe');
+
+// // console.log(person1.greeting());
+
+// //customer constructor
+// function Customer(firstName, lastName, phone, membership){
+//     Person.call(this, firstName, lastName);
+
+//     this.phone = phone;
+//     this.membership = membership;
+// }
+
+// //Inherit the Person prototype methods
+// Customer.prototype = Object.create(Person.prototype);
+
+// //make customer.prototype return Customer()
+// Customer.prototype.constructor = Customer;
+
+// //create customer
+// const customer1 = new Customer('Tom', 'Smith', '555-5555', 'Standard');
+
+// console.log(customer1);
+
+// //Customer greeting
+// Customer.prototype.greeting = function(){
+//     return `Hello there ${this.firstName} ${this.lastName}, welcome to our company!`;
+// }
+
+// console.log(customer1.greeting());
